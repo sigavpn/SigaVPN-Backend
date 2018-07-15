da425af109d936d9458c2f4b46f1249872a1baba
sigavpn <34906324+sigavpn@users.noreply.github.com>
2018-07-15 03:05:49 +0000

    Update generate.php

--- generate.php
+++ generate.php
@@ -1,7 +1,7 @@
 <?php
 // WARNING: This code is kinda messy. Make sure this file is rate limited or protected from HTTP floods somehow.
 error_reporting(0);
-$string=shell_exec("sudo sshpass -p [openvpn server password] ssh root@[openvpn server ip] 'sh /var/www/html/auto.sh' 2>&1"); 
+$string=shell_exec("sudo sshpass -p [openvpn server password] ssh [username]@[openvpn server ip] 'sh /var/www/html/auto.sh' 2>&1"); 
 $vpn=strstr($string, 'http');
 $final=substr($vpn,0,46);
 header('Location: '.$final);
