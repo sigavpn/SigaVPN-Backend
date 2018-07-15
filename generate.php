f6b39aa544b999a2a7bb79dc79d39e45c42513f2
sigavpn <34906324+sigavpn@users.noreply.github.com>
2018-07-15 03:59:17 +0000

    Update generate.php

--- generate.php
+++ generate.php
@@ -1,6 +1,6 @@
 <?php
 error_reporting(0);
-include '/var/ssh.php';
+include '/var/sshkeys.php';
 $string=shell_exec("sudo sshpass -p ".$passwd." ssh [username]@[openvpn server ip] 'sh /var/www/html/auto.sh' 2>&1"); 
 $vpn=strstr($string, 'http');
 $final=substr($vpn,0,46);
