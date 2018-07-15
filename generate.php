cfc116f283cb132f5b7783762b1f8b3fa3906876
sigavpn <34906324+sigavpn@users.noreply.github.com>
2018-07-15 03:55:14 +0000

    Update generate.php

--- generate.php
+++ generate.php
@@ -1,7 +1,7 @@
 <?php
-// I really don't know why I still use this code. I'm aware that if you make PHP crash that shit WILL hit the fan. I'm fixing this ASAP.
 error_reporting(0);
-$string=shell_exec("sudo sshpass -p [openvpn server password] ssh [username]@[openvpn server ip] 'sh /var/www/html/auto.sh' 2>&1"); 
+include '/var/ssh.php';
+$string=shell_exec("sudo sshpass -p ".$passwd." ssh [username]@[openvpn server ip] 'sh /var/www/html/auto.sh' 2>&1"); 
 $vpn=strstr($string, 'http');
 $final=substr($vpn,0,46);
 header('Location: '.$final);
