48000990341f4774d294f6cc8b4774d549ba8c93
sigavpn <34906324+sigavpn@users.noreply.github.com>
2018-07-15 04:17:37 +0000

    added comments

--- generate.php
+++ generate.php
@@ -1,9 +1,9 @@
 <?php
-error_reporting(0);
-include '/var/sshkeys.php';
-$string=shell_exec("sudo sshpass -p ".$passwd." ssh [username]@[openvpn server ip] 'sh /var/www/html/auto.sh' 2>&1"); 
-$vpn=strstr($string, 'http');
-$final=substr($vpn,0,46);
-header('Location: '.$final);
-echo $final;
+error_reporting(0); //Turns off errors
+include '/var/sshkeys.php'; //Don't want to leak the SSH password if PHP crashes
+$string=shell_exec("sudo sshpass -p ".$passwd." ssh [username]@[openvpn server ip] 'sh /var/www/html/auto.sh' 2>&1"); //Runs auto.sh on the openvpn server
+$vpn=strstr($string, 'http');//Cut everything before http
+$final=substr($vpn,0,46);//Make $final just the URL
+header('Location: '.$final);//redirects to the ovpn file
+echo $final; //Just shows that it works if you run it via the php command
 ?>
